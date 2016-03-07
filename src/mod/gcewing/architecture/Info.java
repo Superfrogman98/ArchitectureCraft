19375325df4e54a0bf9c45c0192f5c1f75d6e7b0
Gregory Ewing <greg.ewing@canterbury.ac.nz>
2016-03-08 12:32:36 +1300

    Fix cladding of stairs inner corner

--- src/mod/gcewing/architecture/Info.java
+++ src/mod/gcewing/architecture/Info.java
@@ -7,7 +7,7 @@ public class Info {
 
     static final String modName = "ArchitectureCraft";
     static final String modID = "ArchitectureCraft";
-    static final String versionNumber = "1.3.4";
+    static final String versionNumber = "1.3.5";
     static final String versionBounds = "[1.3,1.4)";
     static final String acceptedMinecraftVersions = "1.7.10";
 
