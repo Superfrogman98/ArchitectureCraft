8b5512f5ddf355d9417f810c8d507192c88acd85
Superfrogman98 <superfrogman98m@gmail.com>
2024-09-07 16:18:45 -0500

    Update src/main/java/gcewing/architecture/common/item/ItemGlowBrush.java
    Co-authored-by: Alexander Anishin <oneeyemaker@gmail.com>

--- src/main/java/gcewing/architecture/common/item/ItemGlowBrush.java
+++ src/main/java/gcewing/architecture/common/item/ItemGlowBrush.java
@@ -32,18 +32,22 @@ public class ItemGlowBrush extends ItemArchitecture {
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side,
             float hitX, float hitY, float hitZ) {
         TileShape te = TileShape.get(world, pos);
-        if (te != null && te instanceof TileShape) {
+        if (te != null) {
             NBTTagCompound shape = new NBTTagCompound();
             te.writeToNBT(shape);
-            if (!player.isSneaking()) {
-                // set the block to the glow variant, and set it's meta to 15 for it to actual give off light
-                world.setBlock(pos.x, pos.y, pos.z, ArchitectureCraft.content.blockShapeSE, 15, 3);
-                world.getTileEntity(pos.x, pos.y, pos.z).readFromNBT(shape);
-            } else {
+            if (player.isSneaking()) {
                 // set the block to the non-glowing variant, and set it's meta to 0 for it to give no light
                 world.setBlock(pos.x, pos.y, pos.z, ArchitectureCraft.content.blockShape, 0, 3);
-                world.getTileEntity(pos.x, pos.y, pos.z).readFromNBT(shape);
+            } else {
+                // set the block to the glow variant, and set it's meta to 15 for it to actual give off light
+                world.setBlock(pos.x, pos.y, pos.z, ArchitectureCraft.content.blockShapeSE, 15, 3);
             }
+            TileShape newTile = TileShape.get(world, pos);
+            if (newTile != null) {
+                newTile.readFromNBT(shape);
+            }
+            return true;
+        }
             return true;
         }
         return false;
